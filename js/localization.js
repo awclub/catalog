3981638317c92ed821adadfa5ed2d8e6bdbbd1f6
itbeard <iamitbeard@gmail.com>
2024-01-06 18:41:27 +0100

    update localizations

--- js/localization.js
+++ js/localization.js
@@ -7,5 +7,8 @@ export function updateStaticLocalizations(localization) {
     document.getElementById('search-box').placeholder = localization.searchPlaceholder;
     document.getElementById('title').textContent = localization.headerTitle;
     document.getElementById('header').textContent = localization.headerTitle;
+    document.getElementById('podcast-link').textContent = localization.podcastLinkText;
+    document.getElementById('podcast-chat-link').textContent = localization.podcastChatLinkText;
+    document.getElementById('github-code-link').textContent = localization.githubCodeLinkText;
     document.title = localization.headerTitle;
 }
