81245c956bc369424b6a03634325874bd1dbf3fa
itbeard <iamitbeard@gmail.com>
2024-01-07 02:01:01 +0100

    Update styles.css link and fix title in localization.js

--- js/localization.js
+++ js/localization.js
@@ -5,11 +5,10 @@ export async function loadLocalization(currentLanguage) {
 
 export function updateStaticLocalizations(localization) {
     document.getElementById('search-box').placeholder = localization.searchPlaceholder;
-    document.getElementById('title').textContent = localization.title;
     document.getElementById('header').innerHTML = localization.header;
     document.getElementById('podcast-link').textContent = localization.podcastLinkText;
     document.getElementById('podcast-chat-link').textContent = localization.podcastChatLinkText;
     document.getElementById('github-code-link').textContent = localization.githubCodeLinkText;
     document.getElementById('reset-button').innerText = localization.resetButtonText;
-    document.title = localization.headerTitle;
+    document.title = localization.title;
 }
