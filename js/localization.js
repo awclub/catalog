591dfe8eec9aa2dc749c88b3b38f5cc36394463b
itbeard <iamitbeard@gmail.com>
2024-01-07 01:48:31 +0100

    Update header and title localization

--- js/localization.js
+++ js/localization.js
@@ -5,8 +5,8 @@ export async function loadLocalization(currentLanguage) {
 
 export function updateStaticLocalizations(localization) {
     document.getElementById('search-box').placeholder = localization.searchPlaceholder;
-    document.getElementById('title').textContent = localization.headerTitle;
-    document.getElementById('header').textContent = localization.headerTitle;
+    document.getElementById('title').textContent = localization.title;
+    document.getElementById('header').innerHTML = localization.header;
     document.getElementById('podcast-link').textContent = localization.podcastLinkText;
     document.getElementById('podcast-chat-link').textContent = localization.podcastChatLinkText;
     document.getElementById('github-code-link').textContent = localization.githubCodeLinkText;
