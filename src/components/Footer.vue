dc949c3d33a03bf95c7df5eedec9e212a2cd0b01
gamezovladislav <gamezo1997@gmail.com>
2024-01-21 13:04:11 +0100

    #18. Footer localization

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,9 +2,9 @@
 <footer>
     <div>
         <a href="https://itbeard.com/aia" target="_blank" id="podcast-link">AIA Podcast</a>
-        <a href="https://t.me/aiapodcast" target="_blank" id="podcast-chat-link">Podcast chat</a>
+        <a href="https://t.me/aiapodcast" target="_blank" id="podcast-chat-link">{{ $t('podcastChatLinkText') }}</a>
         <a href="https://aw.club" target="_blank" >AW Club</a>
-        <a href="https://github.com/awclub/catalog" target="_blank" id="github-code-link">Code on Github</a>
+        <a href="https://github.com/awclub/catalog" target="_blank" id="github-code-link">{{ $t('githubCodeLinkText') }}</a>
     </div>
 </footer>
 </template>
