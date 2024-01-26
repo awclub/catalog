79709f5e434f6c2e1b189ae325217e9dd7e66312
itbeard <iamitbeard@gmail.com>
2024-01-26 21:23:52 +0100

    Fix of 404 for GP

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ServiceView from '../views/ServiceView.vue'
 
 const router = createRouter({
-	history: createWebHashHistory(import.meta.env.BASE_URL),
+	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
 		{
 			path: '/',
