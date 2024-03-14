6e5de590c36646f652cd87121922b15254de7950
gamezovladislav <gamezo1997@gmail.com>
2024-03-14 21:21:23 +0100

    27. migrated searchText filter

--- src/stores/rootFilterStore.js
+++ src/stores/rootFilterStore.js
@@ -5,6 +5,7 @@ export const KEYWORDS = {
 	ORDER: 'sortingOrder',
 	TAGS: 'selectedTags',
 	RANK: 'selectedRanks',
+	TEXT: 'searchText'
 };
 
 const _initState = () => {
@@ -13,6 +14,7 @@ const _initState = () => {
 		[ KEYWORDS.ORDER ]: localStorage.getItem( KEYWORDS.ORDER ),
 		[ KEYWORDS.TAGS ]: JSON.parse(localStorage.getItem( KEYWORDS.TAGS )) || [],
 		[ KEYWORDS.RANK ]: localStorage.getItem( KEYWORDS.RANK ) || '0',
+		[ KEYWORDS.TEXT ]: '',
 	};
 };
 
@@ -25,19 +27,28 @@ export const useRootFilterStore = defineStore('rootFilterStore', ({
 		order: state => state[KEYWORDS.ORDER],
 		tags: state => state[KEYWORDS.TAGS],
 		rank: state => state[KEYWORDS.RANK],
+		text: state => state[KEYWORDS.TEXT]
 	},
 	actions: {
 		setLang(lang) {
+			this[ KEYWORDS.LANG ] = lang;
 			_saveChanges(KEYWORDS.LANG, lang);
 		},
 		setOrder(order) {
+			this[ KEYWORDS.ORDER ] = order;
 			_saveChanges(KEYWORDS.ORDER, order);
 		},
 		setTags(tags) {
+			this[ KEYWORDS.TAGS ] = tags;
 			_saveChanges(KEYWORDS.TAGS, JSON.stringify(tags || []));
 		},
 		setRank(rank) {
+			this[ KEYWORDS.RANK ] = rank;
 			_saveChanges(KEYWORDS.RANK, rank);
+		},
+		setSearchText(text) {
+			this[ KEYWORDS.TEXT ] = text;
+			// we don't store the search text in localStorage, on the other side we need to have searchText in this cumulative store.
 		}
 	}
 }));
